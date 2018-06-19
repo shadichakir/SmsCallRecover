fa1797cfe7454d05a2082bf25dfbb9448b02d9f7
shadichakir <35115107+shadichakir@users.noreply.github.com>
2018-06-19 13:31:48 +0100

    first commit

--- app/src/main/java/dev/justforyou/smscallrecover/MainActivity.java
+++ app/src/main/java/dev/justforyou/smscallrecover/MainActivity.java
@@ -1,25 +1,17 @@
 package dev.justforyou.smscallrecover;
 
 
-import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -40,88 +32,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
-    InterstitialAd mInterstitialAd;
-    private InterstitialAd interstitial;
-    Button btnbegin;
-    TextView t1,t2;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        try {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            //Admob
-            AdRequest adRequest = new AdRequest.Builder().build();
-            checkForConsent();
-            // Prepare the Interstitial Ad
-            interstitial = new InterstitialAd(MainActivity.this);
-            interstitial.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-            interstitial.loadAd(adRequest);
-            interstitial.setAdListener(new AdListener() {
-                public void onAdLoaded() {
-                    displayInterstitial();
-                }
-            });
-            Button btndpd = findViewById(R.id.pdp);
-            btndpd.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (ConsentInformation.getInstance(getBaseContext())
-                            .isRequestLocationInEeaOrUnknown()) {
-                        requestConsent();
-                    }else{
-                        Toast.makeText(getApplicationContext(),"This option is available only for European Users",Toast.LENGTH_LONG).show();}
-                }
-            });
-            btnbegin = findViewById(R.id.beginBtn);
-            btnbegin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    if(isNetworkAvailable())
-                    {
-                        Intent i = new Intent(getBaseContext(), AppUsedActivity.class);
-                        startActivity(i);
-                    }
-                    else
-                    {
-                        Toast.makeText(getApplicationContext(),"المرجو ربط الاتصال بالانترنت !",Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-            t1 = findViewById(R.id.priv);
-            t1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String url = "https://sites.google.com/view/justforyou-privacypolicy/accueil";
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(url));
-                    startActivity(i);
-                }
-            });
-            t2 = findViewById(R.id.rat);
-            t2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Uri uri = Uri.parse("market://details?id=" + getPackageName());
-                    Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-                    // To count with Play market backstack, After pressing back button,
-                    // to taken back to our application, we need to add following flags to intent.
-                    goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
-                            Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
-                            Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                    try {
-                        startActivity(goToMarket);
-                    } catch (ActivityNotFoundException e) {
-                        startActivity(new Intent(Intent.ACTION_VIEW,
-                                Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
-                    }
-                }
-            });
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-        }
-    }
+    AlertDialog.Builder builder;
+    AlertDialog dialog;
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -133,6 +45,117 @@ public class MainActivity extends AppCompatActivity {
         if (interstitial.isLoaded()) {
             interstitial.show();
         }
+    }
+    private InterstitialAd interstitial;
+    Button btnbegin;
+    TextView t1,t2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Admob
+        checkForConsent();
+        AdRequest adRequest = new AdRequest.Builder().build();
+        // Prepare the Interstitial Ad
+        AdView mAdView = findViewById(R.id.ad_view);
+        mAdView.loadAd(adRequest);
+        interstitial = new InterstitialAd(MainActivity.this);
+        interstitial.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        interstitial.loadAd(adRequest);
+        interstitial.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                displayInterstitial();
+            }
+        });
+        btnbegin = findViewById(R.id.beginBtn);
+        btnbegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isNetworkAvailable())
+                {
+                    Intent i = new Intent(getBaseContext(), AppUsedActivity.class);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"المرجو ربط الاتصال بالانترنت !",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        t1 =findViewById(R.id.priv);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                privacyPolicyEvent();
+
+            }
+        });
+        builder = new AlertDialog.Builder(this);
+        builder.setMessage("By using this application, you represent that you have read our Privacy Policy");
+        builder.setCancelable(true);
+
+        builder.setNegativeButton(
+                "Privacy Policy",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://sites.google.com/view/justforyou-privacypolicy/accueil")));
+                    }
+                });
+
+        builder.setPositiveButton(
+                "Continue",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        dialog = builder.create();
+        t2 = findViewById(R.id.rat);
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("market://details?id=" + getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                }
+            }
+        });
+        Button pers =findViewById(R.id.personnal);
+        pers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ConsentInformation.getInstance(getBaseContext())
+                        .isRequestLocationInEeaOrUnknown()) {
+                    requestConsent();
+                }else{
+                    Toast.makeText(getApplicationContext(),"This option is available only for European Users",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        if (ConsentInformation.getInstance(getBaseContext())
+                .isRequestLocationInEeaOrUnknown()) {
+            requestConsent();
+        }else{
+            Toast.makeText(getApplicationContext(),"This option is available only for European Users",Toast.LENGTH_LONG).show();
+        }
+    }
+    private void showform(){
+        if (form!=null){
+            Log.d(TAG,"show ok");
+            form.show();
+        }
+    }
+    public void privacyPolicyEvent(){
+        dialog.show();
     }
     //======================================== GDBR ADS ======================
     private AdView mAdView;
@@ -181,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
         URL privacyUrl = null;
         try {
             // TODO: Replace with your app's privacy policy URL.
-            privacyUrl = new URL("your privacy link");
+            privacyUrl = new URL("  https://sites.google.com/view/justforyou-privacypolicy/accueil");
         } catch (MalformedURLException e) {
             e.printStackTrace();
             // Handle error.
@@ -242,7 +265,6 @@ public class MainActivity extends AppCompatActivity {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
-
 
     }
 
